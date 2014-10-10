c04fd9c306c74a3de059a44ff28f185171196f10
Joshua Sullivan <jsulliva@nerdery.com>
2014-10-10 16:58:17 -0500

    Yet another massive reorganization following Ray Wenderlich tutorial to produce an iOS 7-compatible Framework from the "BuildFramework" aggregate target.

--- CoreImageFilterEnumeration/CoreImageFilterEnumeration.h
+++ CoreImageFilterEnumeration/CoreImageFilterEnumeration.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 Joshua Sullivan. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+@import Foundation;
 
 //! Project version number for CoreImageFilterEnumeration.
 FOUNDATION_EXPORT double CoreImageFilterEnumerationVersionNumber;
